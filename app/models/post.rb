7b634f875a46588ce01c21e9d0d1234d6570635c
Irene <irenemanningm@gmail.com>
2023-07-10 02:06:30 -0500

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :must_be_clickbait
+
+    def must_be_clickbait
+        valid_titles = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+        valid_titles.each do |s| 
+            if title && title.include?(s)
+                return :title
+            end
+        end
+        return errors.add(:title, "Not valid title.")
+    end
 end
